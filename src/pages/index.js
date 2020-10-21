5dc280fb93d0d0acfc55cdbec4ece62a2a9f4e80
will greer <willfromspace@gmail.com>
2020-10-21 02:35:09 -0700

    Part 4

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,26 @@
-import React from "react"
-import Layout from "../components/layout"
+import React from "react";
+import Layout from "../components/layout";
+
+//Context
+import { useGlobalStateContext, useGlobalDispatchContext } from '../context/globalContext';
+
+//Components
+import HomeBanner from '../components/homePage/HomeBanner';
 
 const IndexPage = props => {
-  return <Layout>Hey guys hope you are ready to learn!</Layout>
-}
+  const { currentTheme, cursorStyles } = useGlobalStateContext();
+  const dispatch = useGlobalDispatchContext();
 
-export default IndexPage
+  const onCursor = cursorType => {
+    cursorType = (cursorStyles.includes(cursorType) && cursorType) || false;
+    dispatch({
+      type: 'CURSOR_TYPE', cursorType: cursorType
+    });
+  };
+
+  return <Layout>
+    <HomeBanner onCursor={onCursor} />
+  </Layout>;
+};
+
+export default IndexPage;
